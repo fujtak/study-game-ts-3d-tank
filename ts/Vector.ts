9213778d616b701dbe03b1c9d8d7b14bb6e8a062
fujtak <fuj.eng23@gmail.com>
2026-04-05 16:38:27 +0900

    add: Vector

---
+++ ts/Vector.ts
@@ -0,0 +1,12 @@
+
+export class Vector {
+  readonly x: number;
+  readonly y: number;
+  readonly z: number;
+
+  constructor(x: number, y: number, z: number) {
+    this.x = x
+    this.y = y
+    this.z = z
+  }
+}
